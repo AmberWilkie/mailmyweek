e315dbd70d28e731452cd9781922102e1cf47263
Amber <amber@amberwilkie.com>
2016-12-17 10:24:05 +0100

    Add non-working nav bar with login and out links

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,28 @@ class User < ApplicationRecord
     user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-        user = User.create(name: data["name"],
-           email: data["email"],
-           password: Devise.friendly_token[0,20]
-        )
-    end
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
     user
+  end
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    if User.admins.include?(data.email)
+      user = User.find_by(email: data.email)
+      if user
+        user.provider = access_token.provider
+        user.uid = access_token.uid
+        user.token = access_token.credentials.token
+        user.save
+        user
+      else
+        redirect_to new_user_registration_path, notice: "Error."
+      end
+    end
   end
 end
