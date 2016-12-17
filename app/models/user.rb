1297aa8c69162d26db283f9097aced14528cc833
Amber <amber@amberwilkie.com>
2016-12-17 10:58:50 +0100

    Set google auth token, provider and uid from oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
+      user = User.find_by(email: data.email)
+      if user
+        user.provider = access_token.provider
+        user.uid = access_token.uid
+        user.token = access_token.credentials.token
+        user.save
+        user
+      else
+        redirect_to new_user_registration_path, notice: "Error."
+      end
 
     # Uncomment the section below if you want users to be created if they don't exist
     # unless user
@@ -19,19 +29,20 @@ class User < ApplicationRecord
     user
   end
 
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    if User.admins.include?(data.email)
-      user = User.find_by(email: data.email)
-      if user
-        user.provider = access_token.provider
-        user.uid = access_token.uid
-        user.token = access_token.credentials.token
-        user.save
-        user
-      else
-        redirect_to new_user_registration_path, notice: "Error."
-      end
-    end
-  end
+  # def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+  #   binding.pry
+  #   data = access_token.info
+  #   if User.admins.include?(data.email)
+  #     user = User.find_by(email: data.email)
+  #     if user
+  #       user.provider = access_token.provider
+  #       user.uid = access_token.uid
+  #       user.token = access_token.credentials.token
+  #       user.save
+  #       user
+  #     else
+  #       redirect_to new_user_registration_path, notice: "Error."
+  #     end
+  #   end
+  # end
 end
