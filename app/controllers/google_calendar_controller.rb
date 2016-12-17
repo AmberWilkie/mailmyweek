3aff24789d52eb1aa071e34d9bb6ad534c74585c
Amber <amber@amberwilkie.com>
2016-12-17 14:20:33 +0100

    Properly display date and time for calendar events

--- app/controllers/google_calendar_controller.rb
+++ app/controllers/google_calendar_controller.rb
@@ -5,5 +5,6 @@ class GoogleCalendarController < ApplicationController
     @calendars = JSON.parse @list.response.env.body
     events = @calendar.get_calendar_event(@calendars['items'][10]['id'])
     @event_list = JSON.parse events.response.env.body
+# binding.pry
   end
 end
